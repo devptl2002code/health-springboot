d788f06bd1627dc25650d0915c3936cda15212f8
Dev Patel <devptl2002.code@gmail.com>
2024-05-24 23:30:54 +0530

    Add CRUD methods in controller

--- src/main/java/com/hmp/health/controller/UserController.java
+++ src/main/java/com/hmp/health/controller/UserController.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping(value = "user")
 @CrossOrigin(origins = "http://localhost:4200")
@@ -20,6 +22,31 @@ public class UserController {
     @ResponseStatus(value = HttpStatus.CREATED)
     public User saveUser (@RequestBody User user){
         return  this.userService.insert(user);
+    }
+
+    @GetMapping
+    @ResponseStatus(value = HttpStatus.OK)
+    public List<User> findAllUsers(){
+        return this.userService.findAll();
+    }
+
+    @GetMapping(value = "/{id}")
+    @ResponseStatus(value = HttpStatus.OK)
+    public User findById(@PathVariable int id){
+        return this.userService.findById(id);
+    }
+
+
+    @PutMapping(value = "/{id}")
+    @ResponseStatus(value = HttpStatus.OK)
+    public User updateUser(@PathVariable int id, @RequestBody User user){
+        return this.userService.updateUser(id, user);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    public void deleteUser(@PathVariable int id){
+        this.userService.delete(id);
     }
 
 
